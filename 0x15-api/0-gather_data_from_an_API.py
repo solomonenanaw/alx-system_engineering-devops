dc79940487fabef057252033bdfac1743e2d63ba
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-01-17 04:55:04 +0300

    Update 0-gather_data_from_an_API.py
    access to do list

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Returns to-do list information of a given employee ID"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [task.get("title")
+                 for task in todos if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(done)) for done in completed]
