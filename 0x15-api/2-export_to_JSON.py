129f1459b43d4cc842c9fd18d54da4115fa8863d
solomonenanaw <sekewbeg@gmail.com>
2023-02-01 11:50:36 +0000

    updated document

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""Exports to-do list information of a given employee ID JSON file"""
-import json
-import requests
-import sys
+"""This module contains a python script that interacts with the REST api.
+The script exports a given employee data into a json file format"""
+from requests import get
+from sys import argv
+from json import dump
+
 
 if __name__ == "__main__":
-    usrid = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(usrid)).json()
-    usrname = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": usrid}).json()
+    user_info = get('https://jsonplaceholder.typicode.com/users/{}'.
+                    format(argv[1])).json()
+    user_todos = get('https://jsonplaceholder.typicode.com/todos/',
+                     params={"userId": argv[1]}).json()
 
-    with open("{}.json".format(usrid), "w") as jsonfl:
-        json.dump({usrid: [{
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-            "username": usrname
-        } for task in todos]}, jsonfl)
+    USER_ID = str(user_info.get('id'))
+    USERNAME = user_info.get('username')
+
+    list_info = []
+
+    for task in user_todos:
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+        task_info = {"task": TASK_TITLE, "completed": TASK_COMPLETED_STATUS,
+                     "username": USERNAME}
+        list_info.append(task_info)
+
+    content = {USER_ID: list_info}
+
+    file_name = USER_ID + ".json"
+
+    with open(file_name, "w", encoding="utf8") as f:
+        dump(content, f)
