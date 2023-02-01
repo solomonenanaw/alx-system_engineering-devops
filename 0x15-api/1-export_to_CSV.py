bea1dc3b9d8786c019c2d5e38e7153c811cceddd
solomonenanaw <sekewbeg@gmail.com>
2023-02-01 11:42:13 +0000

    edited document

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-import csv
-import requests
-import sys
+"""This module contains a python script that interacts with the REST api.
+The script exports a given employee data into a CSV file format"""
+from requests import get
+from sys import argv
+from csv import writer, QUOTE_ALL
+
 
 if __name__ == "__main__":
-    usrid = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(usrid)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": usrid}).json()
+    user_info = get('https://jsonplaceholder.typicode.com/users/{}'.
+                    format(argv[1])).json()
+    user_todos = get('https://jsonplaceholder.typicode.com/todos/',
+                     params={"userId": argv[1]}).json()
 
-    with open("{}.csv".format(usrid), "w", newline="") as csvfl:
-        writer = csv.writer(csvfl, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [usrid, username, task.get("completed"), task.get("title")]
-        ) for task in todos]
+    USER_ID = str(user_info.get('id'))
+    USERNAME = user_info.get('username')
+
+    for task in user_todos:
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+
+        info_list = [USER_ID, USERNAME, TASK_COMPLETED_STATUS,
+                     TASK_TITLE]
+        file_name = USER_ID + '.csv'
+
+        with open(file_name, "a") as f:
+            w = writer(f, quoting=QUOTE_ALL)
+            w.writerow(info_list)
