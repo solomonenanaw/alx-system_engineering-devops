6cf88d6e581e036af2761f0ef0a747634f28c59a
solomonenanaw <sekewbeg@gmail.com>
2023-02-02 18:52:08 +0000

    all

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -51,4 +51,4 @@ def count_words(subreddit, word_list, after="", count=[]):
                 if (count[i] > 0) and i not in save:
                     print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
-            count_words(subreddit, word_list, after, count)
+           u count_words(subreddit, word_list, after, count)
